720a2dd354cbdd9ced08222f38fcd97ed73fc0c8
theyoungmaker <hello@theyoungmaker.com>
2024-02-15 16:44:04 +0800

    Add blackbox check_youtube_url function for regex

---
+++ check_youtube_url.py
@@ -0,0 +1,10 @@
+import re
+
+def is_valid_url(url):
+    # Basic validation for a YouTube URL
+    youtube_regex = (
+        r'(https?://)?(www\.)?'
+        '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+        '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+    youtube_regex_match = re.match(youtube_regex, url)
+    return bool(youtube_regex_match)
